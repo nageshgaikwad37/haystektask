7a03dea5108ab7ebb1da46d918135ac1e832fb07
nageshgaikwad37 <107549162+nageshgaikwad37@users.noreply.github.com>
2023-02-28 09:59:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'haystektask';
+  array:any= [];
+  indexNumber:number= 0;
+  baseUrl:string = environment.baseurl;
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+    this.getData()
+    
+    
+  }
+
+  getData(){
+    this.http.get(this.baseUrl).subscribe(res => {
+      this.array = res
+    })
+  }
+
+  onnext(){
+    this.indexNumber = (this.indexNumber + 1) % this.array.length;
+  }
 }
